8ed0d322f310690aa750036f29ecf3fefd4862aa
Luna <47433349+2Luna@users.noreply.github.com>
2023-05-18 23:09:52 +0200

    Update MythicKey.lua
    ## Update - Version 0.1.8
    
    In this update, we have introduced new features and improvements to enhance your MythicKey addon experience. Here's what's included in this version:
    
    ### New Features
    
    - **Enhanced Keystone Insertion**: We have optimized the key insertion process to ensure a smoother and more reliable experience. The addon will now automatically insert the appropriate Mythic+ keystone into the Keystone Frame when it is opened.
    
    - **Ready Check Integration**: We have added a ready check feature to MythicKey. When the key is inserted, a ready check will be automatically initiated in your group. This allows you to quickly determine if everyone is ready to start the dungeon run.
    
    - **Centered Text Output**: Messages related to key insertion, ready check, and other actions will now be displayed in the center of the screen, providing better visibility and ensuring important information doesn't go unnoticed.
    
    ### Improvements
    
    - **Mythic+ Timer Enhancements**: The Mythic+ timer has been refined with improved accuracy and additional display options. Now you can track the remaining time more effectively and stay on top of your dungeon run.
    
    - **Bug Fixes**: We have addressed several bugs reported by the community, improving the overall stability and performance of the addon.
    
    We appreciate your feedback and support in shaping this update. If you encounter any issues or have suggestions for further improvements, please let us know by opening an issue on the [Issues](link-to-issues) page.
    
    To update your MythicKey addon, please follow the installation instructions in the [README](link-to-readme) file.
    
    Thank you for using MythicKey, and we hope this update enhances your Mythic+ dungeon experience even further!
    
    ## License
    
    MythicKey is released under the [MIT License](link-to-license).

--- MythicKey.lua
+++ MythicKey.lua
@@ -15,7 +15,6 @@ AK:SetScript("OnEvent", function(self, event, addon)
         Frame:HookScript("OnShow", function()
             -- Deklariere Variablen
             local ID, Class, SubClass
-            local foundKeystone = false
 
             -- Iteriere über die Taschen des Spielers
             for bag = 0, NUM_BAG_FRAMES do
@@ -33,25 +32,20 @@ AK:SetScript("OnEvent", function(self, event, addon)
                         if (IDs[ID] or (Class == 5 and SubClass == 1)) then
                             -- Verwende den Gegenstand aus der Tasche
                             C_Container.UseContainerItem(bag, slot)
-
-                            -- Lege den Schlüssel automatisch in den Schlüsselslot
-                            if ChallengesKeystoneFrame.SlotContainer.slot then
-                                ChallengesKeystoneFrame.SlotContainer.slot.keyStoneID = ID
-                                ChallengesKeystoneFrame.SlotContainer.slot:SetAttribute("type", "macro")
-                                ChallengesKeystoneFrame.SlotContainer.slot:SetAttribute("macrotext", "/use item:" .. ID .. "\n/use [@player] " .. ID)
-                            end
-
-                            -- Markiere den gefundenen Schlüsselstein
-                            foundKeystone = true
-
-                            break
+                            -- Sende eine Nachricht im Chat, dass der Schlüsselstein eingelegt wurde
+                            print("Keystone inserted and ready for start!")
+                            -- Führe einen Ready-Check mit einer Verzögerung von 0,5 Sekunden aus
+                            C_Timer.After(0.5, function()
+                                -- Sende eine Nachricht im Chat, dass der Ready-Check gesendet wurde
+                                SendChatMessage("Ready check initiated. Please accept.", "PARTY")
+                                -- Führe den Ready-Check aus
+                                DoReadyCheck()
+                                -- Warte auf die READY_CHECK_FINISHED-Nachricht, um festzustellen, ob alle bereit sind
+                                AK:RegisterEvent("READY_CHECK_FINISHED")
+                            end)
+                            return
                         end
                     end
-                end
-
-                -- Beende die Schleife, wenn ein Schlüsselstein gefunden wurde
-                if foundKeystone then
-                    break
                 end
             end
         end)
@@ -67,7 +61,34 @@ AK:SetScript("OnEvent", function(self, event, addon)
             Frame:SetScript("OnDragStop", Frame.StopMovingOrSizing)
         end
 
-        -- Entregistriere das Event, da das Addon erfolgreich geladen wurde
-        self:UnregisterEvent(event)
+        -- Hänge ein Skript an das READY_CHECK_FINISHED
+        AK:RegisterEvent("READY_CHECK_FINISHED")
     end
 end)
+
+-- Erstelle ein Skript, das auf das READY_CHECK_FINISHED-Event reagiert
+AK:SetScript("OnEvent", function(self, event)
+    -- Überprüfe, ob das Event READY_CHECK_FINISHED ausgelöst wurde
+    if event == "READY_CHECK_FINISHED" then
+        -- Überprüfe, ob alle Spieler bereit sind
+        local allReady = true
+        for i = 1, GetNumGroupMembers() do
+            local _, _, _, _, _, readyStatus = GetRaidRosterInfo(i)
+            if readyStatus == nil or readyStatus == 0 then
+                allReady = false
+                break
+            end
+        end
+
+        -- Sende eine Nachricht im Chat, ob alle Spieler bereit sind oder nicht
+        if allReady then
+            print("Everyone is ready!")
+        else
+            print("Not everyone is ready!")
+        end
+
+        -- Unregister das READY_CHECK_FINISHED-Event, um eine erneute Registrierung zu vermeiden
+        AK:UnregisterEvent("READY_CHECK_FINISHED")
+    end
+end)
+
